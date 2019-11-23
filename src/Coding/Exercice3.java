6fdfd0d2cb41d98e2f1e942871cb84de975c920e
Bota Sorina <crisansorinageorgiana@yahoo.com>
2019-11-23 13:12:56 +0200

    Exercice3

---
+++ src/Coding/Exercice3.java
@@ -0,0 +1,32 @@
+package Coding;
+
+import java.util.Scanner;
+
+public class Exercice3 {
+    public static void main(String[] args) {
+        int [] array ;
+
+       array= initializare(100);
+        afisare(array);
+    }
+
+    private static int[] initializare(int limit) {
+       int[] array=new int[limit];
+       int count=0;
+        for (int i = 0; count < limit; i++) {
+            if (i % 11 == 0) {
+             //   System.out.println(count);
+                array[count++]=i;
+               // System.out.println(i); asa sau cu for afis la fel
+            }
+
+        }
+        return array;
+    }
+
+    private static void afisare(int[] array) {
+        for (int i = 0; i <array.length ; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
--- src/Coding/Exercices2.java
+++ src/Coding/Exercices2.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
 
 public class Exercices2 {
     public static void main(String[] args) {
-        Scanner firstScanner = new Scanner(System.in);
-        System.out.println("Enter numer n =");
-        int n = firstScanner.nextInt();
-        System.out.println("Enter numer m =");
-        int m = firstScanner.nextInt();
-        while(n<0||m<0) {
-            System.out.println("Enter numer n =");
-            n = firstScanner.nextInt();
-            System.out.println("Enter numer m =");
-            m = firstScanner.nextInt();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Read first number: ");
+        int n = Integer.parseInt(in.nextLine());
+        System.out.print("Read second number: ");
+        int m = Integer.parseInt(in.nextLine());
+        while (n < 0 || m < 0) {
+            System.out.print("Read first number: ");
+            n = Integer.parseInt(in.nextLine());
+            System.out.print("Read second number: ");
+            m = Integer.parseInt(in.nextLine());
         }
-            numereCitite(n, m);
-        }
+        afisare(n, m);
+    }
 
-    private static void numereCitite(int n, int m) {
-        long x=System.currentTimeMillis();
-        for (int i=1;i<=m;i=i+n){
-         System.out.println(i);
-        if (System.currentTimeMillis()-x>10000)
-        {break;
+    private static void afisare(int n, int m) {
+        long x = System.currentTimeMillis();
+        for (int i = 1; i <=m; i = i + n) {
+            System.out.print(i + " ");
+            if (System.currentTimeMillis() - x > 10000) {
+                break;
+            }
         }
     }
 }
